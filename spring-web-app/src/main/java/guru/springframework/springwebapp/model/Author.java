27d5e32c96a91c2243b591ef208b5b80e39ffa22
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-10 23:48:07 +0100

    Display a List of Authors : * Create Author Controller. * Autowire by constructor Author Repository into Controller. * Create method to bind list of authors to model. * Map path /authors to controller method. * Create Thymeleaf template to display authors.

--- spring-web-app/src/main/java/guru/springframework/springwebapp/model/Author.java
+++ spring-web-app/src/main/java/guru/springframework/springwebapp/model/Author.java
@@ -38,6 +38,14 @@ public class Author {
         this.books = books;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getFirstName() {
         return firstName;
     }
