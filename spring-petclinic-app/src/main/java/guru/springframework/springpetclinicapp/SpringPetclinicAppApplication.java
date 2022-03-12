35ea509a5906d0a0d3ac52f4eaf0094c9cec1c74
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-12 12:46:30 +0100

    Setting up project. closes #2.

---
+++ spring-petclinic-app/src/main/java/guru/springframework/springpetclinicapp/SpringPetclinicAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.springpetclinicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetclinicAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetclinicAppApplication.class, args);
+    }
+
+}
